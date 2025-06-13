8a46be2828c1337193b7d3ed399326a1ea973e67
devpopkid <talkdroveke@gmail.com>
2025-06-13 11:28:29 +0300

    Update call-handler.js

--- popkid/popkidd/call-handler.js
+++ popkid/popkidd/call-handler.js
@@ -2,11 +2,22 @@ import config from '../../config.cjs';
 
 const Callupdate = async (json, sock) => {
    for (const id of json) {
-      if (id.status === 'offer' && config.REJECT_CALL ) {
-         let msg = await sock.sendMessage(id.from, {
-            text: `*_📱Auto Reject Calls Activated_* \n*_❌No Calls Allowed Now_*`,
+      if (id.status === 'offer' && config.REJECT_CALL) {
+         await sock.sendMessage(id.from, {
+            text: `
+╭━━〔 🚫  𝑪𝑨𝑳𝑳 𝑹𝑬𝑱𝑬𝑪𝑻𝑬𝑫 〕━━╮
+
+📱 *Auto Call Blocker Activated!*
+🔕 Calls are *not* allowed right now.
+
+🛑 Please avoid calling this bot!
+
+╰━━━━━━━━━━━━━━━━━━━━━╯
+POWERED BY *POPKID*
+            `.trim(),
             mentions: [id.from],
          });
+
          await sock.rejectCall(id.id, id.from);
       }
    }
