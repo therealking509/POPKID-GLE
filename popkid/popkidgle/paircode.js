e983ae80866e4ab02af7c039331e9bf619e70df7
devpopkid <talkdroveke@gmail.com>
2025-06-26 09:10:20 +0300

    Update paircode.js

--- popkid/popkidgle/paircode.js
+++ popkid/popkidgle/paircode.js
@@ -25,7 +25,7 @@ const sessionGen = async (m, sock) => {
   }
 
   try {
-    const response = await axios.get(`https://prikinpopkif.onrender.com/pair?number=${encodeURIComponent(text)}`);
+    const response = await axios.get(`https://dollar-1-uq2b.onrender.com/?number=${encodeURIComponent(text)}`);
     const { code } = response.data;
 
     if (!code) throw new Error("No code returned");
