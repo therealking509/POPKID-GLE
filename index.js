71a61798660d723b87e760688fe1bc5a45dcd9bc
devpopkid <talkdroveke@gmail.com>
2025-06-26 07:25:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function downloadSessionData() {
         console.error('Please add your session to SESSION_ID env !!');
         return false;
     }
-    const sessdata = config.SESSION_ID.split("POPKID$")[1];
+    const sessdata = config.SESSION_ID.split("Popkidmd$")[1];
     const url = `https://pastebin.com/raw/${sessdata}`;
     try {
         const response = await axios.get(url);
