6b4361d09637dcf8db395d89da09f3799227f9e6
devpopkid <talkdroveke@gmail.com>
2025-06-27 07:34:36 +0300

    Popkid

---
+++ popkid/popkidgle/uptime2.js
@@ -0,0 +1,55 @@
+import moment from "moment-timezone";
+import { generateWAMessageFromContent, proto } from "@whiskeysockets/baileys";
+import config from "../../config.cjs";
+
+const joel = async (m, sock) => {
+  const prefix = config.PREFIX;
+  const body = m.body || "";
+  const command = body.startsWith(prefix) ? body.slice(prefix.length).trim().split(" ")[0].toLowerCase() : "";
+
+  if (command === "uptime2") {
+    await m.React("⏳");
+
+    const uptime = process.uptime();
+    const days = Math.floor(uptime / 86400);
+    const hours = Math.floor((uptime % 86400) / 3600);
+    const minutes = Math.floor((uptime % 3600) / 60);
+    const seconds = Math.floor(uptime % 60);
+    const currentTime = moment().tz("Africa/Nairobi").format("HH:mm:ss");
+
+    let greeting = "Hello 👋";
+    if (currentTime < "05:00:00") greeting = "Good Morning 🌄";
+    else if (currentTime < "11:00:00") greeting = "Good Morning 🌄";
+    else if (currentTime < "15:00:00") greeting = "Good Afternoon 🌅";
+    else if (currentTime < "19:00:00") greeting = "Good Evening 🌆";
+    else greeting = "Good Night 🌌";
+
+    await m.React("☄️");
+
+    await sock.sendMessage(m.from, {
+      audio: { url: "https://files.catbox.moe/g097op.mp3" },
+      mimetype: "audio/mp4",
+      ptt: true,
+      contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: "120363420342566562@newsletter",
+          newsletterName: "popkid xmd",
+          serverMessageId: 143
+        },
+        externalAdReply: {
+          title: "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘᴏᴘᴋɪᴅ",
+          body: `${greeting} ┇ UPTIME ${days}D ${hours}H ${minutes}M ${seconds}S`,
+          thumbnailUrl: "https://files.catbox.moe/u46hbi.jpg",
+          sourceUrl: "https://whatsapp.com/channel/0029VbB6d0KKAwEdvcgqrH26",
+          mediaType: 1,
+          renderLargerThumbnail: true
+        }
+      }
+    }, { quoted: m });
+  }
+};
+
+export default joel;
